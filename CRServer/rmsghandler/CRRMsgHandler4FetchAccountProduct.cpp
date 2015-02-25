bc309dc405dcad7b5c2d99f5f04b2ef13398dd3c
rwang82 <rwang82@gmail.com>
2015-02-25 21:35:29 +0800

    add CRRMsgHandler4FetchAccountProduct.

---
+++ CRServer/rmsghandler/CRRMsgHandler4FetchAccountProduct.cpp
@@ -0,0 +1,147 @@
+#include "stdafx.h"
+#include "CRRMsgHandler4FetchAccountProduct.h"
+#include "CRRCmdDefs.h"
+#include "CRErrCode.h"
+#include "CRSrvMisc.h"
+#include "CRServerRoot.h"
+#include "CRModuleDepot.h"
+#include "CRModuleIdDefs.h"
+#include "CRModuleProductMgr.h"
+#include "CRMOduleAccountMgr.h"
+#include "CRRMsgMaker.h"
+#include "HMNWPServer.h"
+#include "HMCharConv.h"
+#include <string>
+
+CRRMsgHandler4FetchAccountProduct::CRFetchAccountProductResult::CRFetchAccountProductResult() {
+
+}
+
+CRRMsgHandler4FetchAccountProduct::CRFetchAccountProductResult::~CRFetchAccountProductResult() {
+
+}
+
+void CRRMsgHandler4FetchAccountProduct::CRFetchAccountProductResult::_clearAll() {
+	name2pl_map_type::iterator itName2PL, iendName2PL;
+	product_list_type* pProductList = NULL;
+	product_list_type::iterator itProduct, iendProduct;
+	CRProduct* pProduct = NULL;
+
+	iendName2PL = m_mapName2ProductList.end();
+	for ( itName2PL = m_mapName2ProductList.begin(); itName2PL!=iendName2PL; ++itName2PL ) {
+	    pProductList = itName2PL->second;
+	    if ( !pProductList )
+			continue;
+		iendProduct = pProductList->end();
+		for ( itProduct = pProductList->begin(); itProduct!=iendProduct; ++itProduct ) {
+		    pProduct = *itProduct;
+			if ( !pProduct )
+				continue;
+			delete pProduct;
+			pProduct = NULL;
+		}
+		delete pProduct;
+		pProduct = NULL;
+	}
+	m_mapName2ProductList.clear();
+}
+
+CRRMsgHandler4FetchAccountProduct::CRRMsgHandler4FetchAccountProduct() {
+
+}
+
+CRRMsgHandler4FetchAccountProduct::~CRRMsgHandler4FetchAccountProduct() {
+
+}
+
+
+bool CRRMsgHandler4FetchAccountProduct::_parseParams( const Json::Value& jsonRoot, CRFetchAccountProductParam& fapParam ) {
+	const Json::Value& valParams = jsonRoot[ "params" ];
+	if ( !valParams.isObject() )
+		return false;
+	const Json::Value& valUserNameList = valParams[ "username_list" ];
+	if ( !valUserNameList.isArray() )
+		return false;
+	std::string strUTF8;
+	tstring_type tstrTmp;
+	//
+	int nCount = valUserNameList.size();
+	for ( int nIndex = 0; nIndex<nCount; ++nIndex ) {
+	    const Json::Value& valItem = valUserNameList[ nIndex ];
+		if ( !valItem.isString() )
+			continue;
+		strUTF8 = valItem.asString();
+		if ( !UTF8ToTCHAR( strUTF8, tstrTmp  ) )
+			continue;
+		fapParam.m_containerAccountName.push_back( tstrTmp );
+	}
+
+	return true;
+}
+
+void CRRMsgHandler4FetchAccountProduct::accept( const CRRMsgMetaData& rmsgMetaData, const CRRMsgJson* pRMsgJson ) {
+	CRFetchAccountProductParam fapParam;
+	int nErrCode = CRERR_SRV_NONE;
+	Json::Value valAccountProductList;
+	//
+	if ( !_parseParams( pRMsgJson->m_jsonRoot, fapParam ) ) {
+	    _sendFailedAck( rmsgMetaData, CRERR_SRV_PARAM_INVALID );
+		return;
+	}
+	//
+    if ( !_execute( fapParam, valAccountProductList, nErrCode ) ) {
+	    _sendFailedAck( rmsgMetaData, nErrCode );
+	}
+	//
+	_sendSuccessAck( rmsgMetaData, valAccountProductList );
+}
+
+bool CRRMsgHandler4FetchAccountProduct::_execute( const CRFetchAccountProductParam& fapParam, Json::Value& valAccountProductList, int& nErrCode ) {
+	tstr_container_type::const_iterator citAccountName, ciendAccountName;
+	CRModuleProductMgr* pProductMgr = (CRModuleProductMgr*)g_CRSrvRoot.m_pModuleDepot->getModule( ECRMODULE_ID_PRODUCTMGR );
+	CRModuleAccountMgr* pAccountMgr = (CRModuleAccountMgr*)g_CRSrvRoot.m_pModuleDepot->getModule( ECRMODULE_ID_ACCOUNTMGR );
+	const CRAccountBase* pAccount = NULL;
+	const CRAccountData* pAccountData = NULL;
+	std::string strUTF8;
+
+	ciendAccountName = fapParam.m_containerAccountName.end();
+	for ( citAccountName = fapParam.m_containerAccountName.begin(); citAccountName!=ciendAccountName; ++citAccountName ) {
+		pAccount = pAccountMgr->getAccount( *citAccountName, nErrCode );
+		if ( !pAccount )
+			return false;
+		pAccountData = &pAccount->m_data;
+		CRFetchAccountProducts fapItem( pAccountData->m_tstrUserName, 0, pAccountData->m_nCountPublished );
+
+		if ( !TCHARToUTF8( pAccountData->m_tstrUserName, strUTF8 ) ) {
+		    assert( false );
+			continue;
+		}
+  
+		Json::Value& valProducts = valAccountProductList[ strUTF8 ];
+		if ( !pProductMgr->fillAccountProducts2Json( fapItem, valProducts, nErrCode ) )
+			return false;
+	}
+
+	return true;
+}
+
+void CRRMsgHandler4FetchAccountProduct::_sendFailedAck( const CRRMsgMetaData& rmsgMetaData, int nErrCode ) {
+	gfnSendFailedAck( rmsgMetaData, CRCMDTYPE_ACK_FETCH_ACCOUNT_PRODUCTS, nErrCode );
+}
+
+void CRRMsgHandler4FetchAccountProduct::_sendSuccessAck( const CRRMsgMetaData& rmsgMetaData, Json::Value& valAccountProductList ) {
+	Json::Value valParams;
+	std::string strRMsg;
+
+	valParams[ "result" ] = 1;
+	valParams[ "account_products_list" ] = valAccountProductList;
+	
+	CRRMsgMaker::createRMsg( valParams, CRCMDTYPE_ACK_FETCH_ACCOUNT_PRODUCTS, strRMsg );
+
+	//
+	g_CRSrvRoot.m_pNWPServer->send( rmsgMetaData.m_sConnect, (const unsigned char*)strRMsg.c_str(), strRMsg.length() + 1 );
+}
+
+void CRRMsgHandler4FetchAccountProduct::accept( const CRRMsgMetaData& rmsgMetaData, const CRRMsgBinary* pRMsgBinary ) {
+
+}
---
+++ CRServer/rmsghandler/CRRMsgHandler4FetchAccountProduct.h
@@ -0,0 +1,46 @@
+#ifndef __CRRMSGHANDLER4FETCHACCOUNTPRODUCT_H__
+#define __CRRMSGHANDLER4FETCHACCOUNTPRODUCT_H__
+#include "CRRMsg.h"
+#include "CRRMsgHandlerBase.h"
+#include "CRTypes.h"
+#include "CRProduct.h"
+#include <list>
+#include <map>
+
+class CRRMsgHandler4FetchAccountProduct : public CRRMsgHandlerBase{
+private:
+	struct CRFetchAccountProductParam {
+	    tstr_container_type m_containerAccountName; 
+	}; 
+	struct CRFetchAccountProductResult {
+		typedef std::list< CRProduct* > product_list_type;
+	    typedef std::map< tstring_type, product_list_type* > name2pl_map_type;
+		CRFetchAccountProductResult();
+		~CRFetchAccountProductResult();
+
+	private:
+		void _clearAll();
+
+	public:
+		name2pl_map_type m_mapName2ProductList;
+	};
+
+public:
+	CRRMsgHandler4FetchAccountProduct();
+	virtual ~CRRMsgHandler4FetchAccountProduct();
+	
+private:
+	virtual void accept( const CRRMsgMetaData& rmsgMetaData, const CRRMsgJson* pRMsgJson );
+	virtual void accept( const CRRMsgMetaData& rmsgMetaData, const CRRMsgBinary* pRMsgBinary );
+
+private:
+	bool _parseParams( const Json::Value& jsonRoot, CRFetchAccountProductParam& fapParam );
+    bool _execute( const CRFetchAccountProductParam& fapParam, Json::Value& valAccountProductList, int& nErrCode );
+	void _sendSuccessAck( const CRRMsgMetaData& rmsgMetaData, Json::Value& valAccountProductList );
+	void _sendFailedAck( const CRRMsgMetaData& rmsgMetaData, int nErrCode );
+
+};
+
+
+
+#endif //__CRRMSGHANDLER4FETCHACCOUNTPRODUCT_H__
